48c164508b6c7a9bb7d553d4cba93724dad381f7
Viktor <viktor.v.antoniuk@gmail.com>
2021-03-24 16:32:39 +0200

    Index page added

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Bakery.RazorPages.Client.Services;
+using Bakery.RazorPages.Client.Models;
 
 namespace Bakery.RazorPages.Client.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ProductService _productService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<Product> Products { get; private set; } = new List<Product>();
+
+        public Product FeaturedProduct { get; set; }
+
+        public IndexModel(ProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            Products = await _productService.GetProducts();
+            FeaturedProduct = Products.ElementAt(new Random().Next(Products.Count<Product>()));
         }
+
     }
 }
